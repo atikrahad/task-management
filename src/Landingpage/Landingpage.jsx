70bc42470d31c434d06f7bb0b6a2cfbbda207146
unknown <atikrahad1@gmail.com>
2023-12-21 11:50:41 +0600

    adding footer section

--- src/Landingpage/Landingpage.jsx
+++ src/Landingpage/Landingpage.jsx
@@ -1,9 +1,11 @@
+import Footer from "./Footer/Footer";
 import Banner from "./Header/Banner";
 
 const Landingpage = () => {
     return (
         <div>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
